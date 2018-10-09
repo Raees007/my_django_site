7de7b80495495ab44113e0b7bb21c0e3ffd3e3de
Raees Mustafa <r33857328@gmail.com>
2018-10-09 05:29:04 +0500

    Add detail view for each blog post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     # 127.0.0.8000 --> Local
-    path('', views.post_list, name='post_list')
+    # myapp.com --> online
+    path('', views.post_list, name='post_list'),
+
+    # 127.0.0.8000/post/1 --> Local
+    # myapp.com/post/1 --> online
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
