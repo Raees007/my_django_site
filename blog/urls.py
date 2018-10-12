c27a7eae25a79e9c701b54c9d31ce648daef4b91
Raees Mustafa <r33857328@gmail.com>
2018-10-12 07:21:15 +0500

    Add feature to create new Blog Post from frontend.

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
 
     # 127.0.0.8000/post/1 --> Local
     # myapp.com/post/1 --> online
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
+    # 127.0.0.8000/post/new --> Local
+    # myapp.com/post/new --> online
+    path('post/new/', views.post_new, name='post_new'),
 ]
